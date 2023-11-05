3af4ed266147f4fe78fcd018b032b00f0ba2fe3d
OMAREID0 <omareid221b@gmail.com>
2023-11-06 00:36:01 +0200

    task from 0 to 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    list_lin = - len(my_list)
+    for list_lin in range(list_lin, 0):
+        print("{:d}".format(my_list[list_lin]))
+        list_lin = list_lin + 1
